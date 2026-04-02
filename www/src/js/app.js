e7461490ad14b243bbfb37bf70047085a434a7a4
javierhernandezprogramador-stack <javier.hernandez.programador@gmail.com>
2026-04-02 14:08:54 -0600

    Se agregan y eliminan citas al nivel de frontend, se agrego validación de fecha

--- www/src/js/app.js
+++ www/src/js/app.js
@@ -2,6 +2,13 @@ let paso = 1;
 const pasoInicial = 1;
 const pasoFinal = 3;
 
+const cita = {
+    nombre: '',
+    fecha: '',
+    hora: '',
+    servicios: []
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     console.log("iniciando...");
     iniciarApp();
@@ -15,6 +22,9 @@ function iniciarApp() {
     paginaAnterior();
 
     consultarAPI(); //consulta la API de php
+
+    nombreCliente();
+    seleccionarFecha();
 }
 
 function mostrarSeccion() {
@@ -121,6 +131,9 @@ function mostrarServicios(servicios) {
         const servicioDiv = document.createElement('DIV');
         servicioDiv.classList.add('servicio');
         servicioDiv.dataset.idServicio = id;
+        servicioDiv.onclick = function () { //Esto sirve para que JS no la mande a llamar en este momento
+            seleccionarServicio(servicio);
+        }
 
         servicioDiv.appendChild(nombreServicio);
         servicioDiv.appendChild(precioServicio);
@@ -129,3 +142,61 @@ function mostrarServicios(servicios) {
     });
 }
 
+function seleccionarServicio(servicio) {
+    const { id } = servicio;
+    const { servicios } = cita; //extraer arreglo de servicios que esta en cita
+
+    //Identificar al elemento que se le da click
+    const divServicio = document.querySelector(`[data-id-servicio="${id}"]`);
+
+    //Comprobar si un servicio ya fue agregado
+    if (servicios.some(agregado => agregado.id === id)) {//some rrecorre un array y verifica si existe un elemento -> retorna true o false
+        //Eliminarlo
+        cita.servicios = servicios.filter(agregado => agregado.id != id);
+        divServicio.classList.remove('seleccionado');
+    } else {
+        //Agregarlo
+        cita.servicios = [...servicios, servicio];// hago una copia de servicios (info existente), agrega un nuevo servicio y reescribe cita.servicios
+        divServicio.classList.add('seleccionado');
+    }
+    console.log(cita);
+}
+
+function nombreCliente() {
+    cita.nombre = document.querySelector('#nombre').value;
+}
+
+function seleccionarFecha() {
+    const inputFecha = document.querySelector('#fecha');
+    inputFecha.addEventListener('input', function (e) {
+        const dia = new Date(e.target.value).getUTCDay();
+
+        if ([6, 0].includes(dia)) {
+            e.target.value = '';
+            mostrarAlerta('Fines de semanas no permitidos', 'error');
+        } else {
+            cita.fecha = e.target.value;
+        }
+    });
+}
+
+function mostrarAlerta(mensaje, tipo) {
+
+    //Previene que se genere mas de una alerta
+    const alertaPrevia = document.querySelector('.alerta');
+    if (alertaPrevia) return;
+
+    //scripting para crear alerta
+    const alerta = document.createElement('DIV');
+    alerta.textContent = mensaje;
+    alerta.classList.add('alerta');
+    alerta.classList.add(tipo);
+
+    const formulario = document.querySelector('.formulario');
+    formulario.appendChild(alerta);
+
+    //eliminar alerta
+    setTimeout(() => {
+        alerta.remove();
+    }, 3000);
+}
