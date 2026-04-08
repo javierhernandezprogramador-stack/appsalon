a6f2ece81da2ec2796aabd81667bdcc0645f3027
javierhernandezprogramador-stack <javier.hernandez.programador@gmail.com>
2026-04-08 16:59:37 -0600

    termine modulo de servicios

--- www/controllers/ServicioController.php
+++ www/controllers/ServicioController.php
@@ -10,13 +10,19 @@ class ServicioController
 
     public static function index(Router $router)
     {
+        isAdmin();
+
+        $servicios = Servicio::all();
+
         $router->render('servicios/index', [
-            'nombre' => $_SESSION['nombre']
+            'nombre' => $_SESSION['nombre'],
+            'servicios' => $servicios
         ]);
     }
 
     public static function crear(Router $router)
     {
+        isAdmin();
         $servicio = new Servicio();
         $alertas = [];
 
@@ -41,17 +47,42 @@ class ServicioController
 
     public static function actualizar(Router $router)
     {
+        isAdmin();
+        if (!is_numeric($_GET['id'])) return;
+
+        $id = $_GET['id'];
+        $servicio = Servicio::find($id);
+        $alertas = [];
+
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $servicio->sincronizar($_POST);
+
+            $alertas = $servicio->validar();
+
+            if (empty($alertas)) {
+                $servicio->guardar();
+                header('Location: /servicios');
+            }
         }
 
         $router->render('servicios/actualizar', [
-            'nombre' => $_SESSION['nombre']
+            'nombre' => $_SESSION['nombre'],
+            'servicio' => $servicio,
+            'alertas' => $alertas
         ]);
     }
 
-    public static function eliminar(Router $router)
+    public static function eliminar()
     {
+        isAdmin();
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = $_POST['id'];
+            $servicio = Servicio::find($id);
+
+            if ($servicio) {
+                $servicio->eliminar();
+                header('Location: /servicios');
+            }
         }
     }
 }
